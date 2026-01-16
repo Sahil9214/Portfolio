5da43b7a17444d41829124f531ea5d5c578fd9db
Sahil9214 <utkarshsinghal369@gmail.com>
2026-01-16 21:53:16 +0530

    Update sitemap last modified dates and clean up timeline component formatting

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -7,7 +7,7 @@ interface TimelineEntry {
   title: string;
   content: React.ReactNode;
 }
-
+// hello
 export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const ref = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -45,8 +45,9 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           {data.map((item, index) => (
             <div
               key={index}
-              className={`flex justify-start pt-10  md:pt-20 md:gap-10 ${index === 0 ? "pt-20" : "pt-10"
-                }`}
+              className={`flex justify-start pt-10  md:pt-20 md:gap-10 ${
+                index === 0 ? "pt-20" : "pt-10"
+              }`}
             >
               <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
                 <div className="h-10 absolute left-2 md:left-2 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
